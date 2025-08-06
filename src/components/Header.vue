2654a1df6e02c44e4311a5dfc24476af52ec14fa
Jaymar Balansag <balansagjm@gmail.com>
2025-08-06 21:37:43 +0800

    Version 2

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,41 +1,55 @@
 
 <template>
-    <div class="shadow-sm pb-4">
-        <nav class="navbar navbar-expand-md align-top">
-            <div class="container">
-                <a class="navbar-brand fw-bold" href="#">RentaSpace</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav mx-auto gap-5">
-                        <li class="nav-item">
-                            <RouterLink class="nav-link" to="/">Home</RouterLink>
-                        </li>
-                        <li class="nav-item">
-                            <RouterLink class="nav-link" to="/map">Map</RouterLink>
-                        </li>
-                        <li class="nav-item">
-                            <RouterLink class="nav-link" to="/experience">Experience</RouterLink>
-                        </li>
-                    </ul>
-
-                    <div class="d-flex ms-auto">
-                        <button class="btn"><i class="bi bi-person-circle profile"></i></button>
-                    </div>
-                </div>
-            </div>
+    <!-- Navbar on tablet, laptop, desktop -->
+    <div class="container ">
+        <nav class="navbar">
+            <RouterLink class="navbar-brand fw-bold" to="/">RentaSpace</RouterLink>
+            <ul class="nav gap-5 d-md-flex d-none">
+                <li class="nav-item">
+                    <RouterLink class="nav-link text-black" to="/">Home</RouterLink>
+                </li>
+                <li class="nav-item">
+                    <RouterLink class="nav-link text-black" to="/map">Map</RouterLink>
+                </li>
+                <li class="nav-item">
+                    <RouterLink class="nav-link text-black" to="/experience">Experience</RouterLink>
+                </li>
+            </ul>
+            <button class="btn profile" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBothOptions" aria-controls="offcanvasWithBothOptions">
+                <i class="bi bi-list"></i>
+            </button>
         </nav>
+        <offCanvas></offCanvas>
     </div>
 
+    <!-- Bottom nav on mobile -->
+     <div class="container-fluid  fixed-bottom d-md-none d-block bg-white top-shadow">
+        <nav class="navbar justify-content-center">
+            <ul class="nav gap-5">
+                <li class="nav-item">
+                    <RouterLink class="nav-link text-black fw-bold" to="/properties" >Home</RouterLink>
+                </li>
+                <li class="nav-item">
+                    <RouterLink class="nav-link text-black fw-bold" to="/map">Map</RouterLink>
+                </li>
+                <li class="nav-item">
+                    <RouterLink class="nav-link text-black fw-bold" to="/experience">Experience</RouterLink>
+                </li>
+            </ul>
+        </nav>
+     </div>
+
+    
 </template>
 
 <script>
 import { RouterLink } from 'vue-router';
-
-
+import offCanvas from './offCanvas.vue';
     export default {
         name : "Header",
+        components: {
+            offCanvas
+        }
     }
 
 </script>
--- src/views/properties.vue
+++ src/views/properties.vue
@@ -3,7 +3,10 @@
         <PropertyFilter></PropertyFilter>
         <!-- Boarding House -->
         <div class="row mx-3 my-5 gy-3">
-            <h1 class="h2">Boarding House</h1>
+            <div class="container-fluid d-flex ">
+                <h1 class="h2 me-auto">Boarding House</h1>
+                <RouterLink class="link-primary ms-auto">See more <i class="bi bi-three-dots-vertical"></i></RouterLink>
+            </div>
             <div class="col-12 col-sm-6 col-md-4 col-lg-3">
                 <div class="card">
                     <img src="../assets/rental.jpg" alt="" class="card-img-top img-fluid">
@@ -71,7 +74,10 @@
         </div>
         <!-- Hotels -->
         <div class="row mx-3 my-5 gy-3">
-            <h1 class="h2">Hotels</h1>
+            <div class="container-fluid d-flex ">
+                <h1 class="h2 me-auto">Boarding House</h1>
+                <RouterLink class="link-primary ms-auto">See more <i class="bi bi-three-dots-vertical"></i></RouterLink>
+            </div>
             <div class="col-12 col-sm-6 col-md-4 col-lg-3">
                 <div class="card">
                     <img src="../assets/hotel.jpg" alt="" class="card-img-top img-fluid">
